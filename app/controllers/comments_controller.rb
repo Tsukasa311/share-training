081ab6abd31cde1562c081038a834d3bebd68875
Tsukasa311 <70062121+Tsukasa311@users.noreply.github.com>
2021-01-28 11:08:34 +0900

    一部リファクタリング

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,23 @@
 class CommentsController < ApplicationController
+  before_action :set_post_comments
 
   def create
     @comment = Comment.create(comment_params)
-    @post = Post.find(params[:post_id])
-    @comments = @post.comments.includes(:user)
   end
   
   def destroy
     comment = Comment.find(params[:id])
     comment.destroy
-    @post = Post.find(params[:post_id])
-    @comments = @post.comments.includes(:user)
   end
 
   private
 
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
+  end
+
+  def set_post_comments
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments.includes(:user)
   end
 end
