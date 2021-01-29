3cbeb8a3b05d7f050cf84fdd3c3ef2e543f56418
Tsukasa311 <70062121+Tsukasa311@users.noreply.github.com>
2021-01-29 12:53:24 +0900

    README追記

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,6 @@ class Post < ApplicationRecord
   has_many :comments,dependent: :destroy
   has_many :likes,dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
-  has_many :post_tag_relations, dependent: :destroy
-  has_many :tags, through: :post_tag_relations, dependent: :destroy
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :part
