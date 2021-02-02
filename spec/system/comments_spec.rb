99fec37ae496406521a5bcb767bdc5abc9dc5271
Tsukasa311 <70062121+Tsukasa311@users.noreply.github.com>
2021-02-02 14:04:49 +0900

    【結合テスト】コメントができる時

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -2,8 +2,28 @@ require 'rails_helper'
 
 RSpec.describe "コメント機能", type: :system do
   before do
+    @user = FactoryBot.create(:user)
     @post = FactoryBot.create(:post)
-    
+    @comment = FactoryBot.build(:comment)
   end
 
+  it 'ログインしたユーザーは、トレーニング投稿詳細ページでコメントが投稿できる' do
+    # ログインする
+    visit new_user_session_path
+    fill_in 'user_email', with: @user.email
+    fill_in 'user_password', with: @user.password
+    find("input[name='commit']").click
+    expect(current_path).to eq root_path
+    # トレーニング投稿詳細ページに遷移する
+    find(".search_btn").click
+    expect((all(".more")[0].hover)).to have_link '詳細', href: "/posts/#{@post.id}"
+    visit post_path(@post.id)
+    # フォームに情報を入力する
+    fill_in 'comment[text]', with: @comment.text
+    # コメントを送信すると、Commentモデルのカウントが１上がるり、コメントした投稿がページに存在することを確認する
+    expect{
+      find("#comment_submit").click
+      expect(page).to have_content(@comment.text)
+    }.to change { Comment.count }.by(1)
+  end
 end
--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -8,27 +8,27 @@ RSpec.describe "トレーニング投稿", type: :system do
 
   context 'トレーニング投稿ができるとき' do
     it 'ログインしたユーザーは新規投稿できる' do
-    # ログインする
-    visit new_user_session_path
-    fill_in 'user_email', with: @user.email
-    fill_in 'user_password', with: @user.password
-    find("input[name='commit']").click
-    expect(current_path).to eq root_path
-    # 新規投稿ページへのリンクがあることを確認する
-    expect(page).to have_content('Shareする')
-    # 新規投稿ページに移動する
-    visit new_post_path
-    # フォームを入力する
-    fill_in 'post[text]', with: @post.text
-    find("#post_part_id").find("option[value='#{@post.part_id}']").select_option
-    find("#post_equipment_id").find("option[value='#{@post.equipment_id}']").select_option
-    find("#post_place_id").find("option[value='#{@post.place_id}']").select_option
-    # 送信すると、Postモデルのカウントが１上がることを確認する
-    expect{find("input[name='commit']").click}.to change { Post.count }.by(1)
-    # マイページに遷移する
-    expect(current_path).to eq "/users/#{@user.id}"
-    # マイページに先ほど投稿した内容のトレーニングが存在することを確認する
-    expect(page).to have_content(@post.text)
+      # ログインする
+      visit new_user_session_path
+      fill_in 'user_email', with: @user.email
+      fill_in 'user_password', with: @user.password
+      find("input[name='commit']").click
+      expect(current_path).to eq root_path
+      # 新規投稿ページへのリンクがあることを確認する
+      expect(page).to have_content('Shareする')
+      # 新規投稿ページに移動する
+      visit new_post_path
+      # フォームを入力する
+      fill_in 'post[text]', with: @post.text
+      find("#post_part_id").find("option[value='#{@post.part_id}']").select_option
+      find("#post_equipment_id").find("option[value='#{@post.equipment_id}']").select_option
+      find("#post_place_id").find("option[value='#{@post.place_id}']").select_option
+      # 送信すると、Postモデルのカウントが１上がることを確認する
+      expect{find("input[name='commit']").click}.to change { Post.count }.by(1)
+      # マイページに遷移する
+      expect(current_path).to eq "/users/#{@user.id}"
+      # マイページに先ほど投稿した内容のトレーニングが存在することを確認する
+      expect(page).to have_content(@post.text)
     end
   end
 
