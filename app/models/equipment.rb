68a7375bcef9fcb9e17c7a43dfc183ca848eb189
Tsukasa311 <70062121+Tsukasa311@users.noreply.github.com>
2021-02-08 18:22:39 +0900

    Home画像追加

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -2,11 +2,10 @@ class Equipment < ActiveHash::Base
   self.data = [
     { id: 1, name: '身体一つ！' },
     { id: 2, name: 'ダンベル' },
-    { id: 3, name: 'インクラインベンチ' },
-    { id: 4, name: 'ダンベル+インクラインベンチ' },
-    { id: 5, name: 'ヨガマット' },
-    { id: 6, name: 'ジムマシン'},
-    { id: 7, name: 'その他' }
+    { id: 3, name: 'ダンベル+インクラインベンチ' },
+    { id: 4, name: 'ヨガマット' },
+    { id: 5, name: 'ジムマシン'},
+    { id: 6, name: 'その他' }
   ]
 
   include ActiveHash::Associations
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :post do
     text            {Faker::Lorem.words }
     part_id         {Faker::Number.between(from: 1, to: 8)}
-    equipment_id    {Faker::Number.between(from: 1, to: 7)}
+    equipment_id    {Faker::Number.between(from: 1, to: 6)}
     place_id        {Faker::Number.between(from: 1, to: 3)}
    
     association :user
