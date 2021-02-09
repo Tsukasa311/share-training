04c7d4605dec82dd5cceee712c7cbc588775bac0
Tsukasa311 <70062121+Tsukasa311@users.noreply.github.com>
2021-02-09 10:35:43 +0900

    Home画像切り替え

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ require("jquery")
 require("chartkick")
 require("chart.js")
 require("./count")
+require("./home")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
---
+++ app/javascript/packs/home.js
@@ -0,0 +1,14 @@
+let num = 0
+function slideTimer() {
+  const homeTop = document.getElementById("home");
+  const classLists = new Array("home_top1","home_top2","home_top3")
+
+  if (num == 2) {
+    num = 0;
+  }else {
+    num ++;
+  }
+  homeTop.className = classLists[num]
+}
+
+setInterval(slideTimer, 6000);
